674dee1c731ea379b51f252c39cb2f55885bd746
Angel Serrano <angeldavid218@gmail.com>
2022-01-17 20:32:01 -0600

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.getRoutes, name='routes'),
+    path('products/', views.getRoutes, name='products'),
+    path('product/<str:pk>', views.getRoutes, name='product')
+]
+
