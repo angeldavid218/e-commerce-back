7f6f746f899499d018216011319382052c00a9d2
angeldavid218 <angeldavid218@gmail.com>
2022-04-02 17:04:19 -0600

    Place order

--- base/serializers.py
+++ base/serializers.py
@@ -17,6 +17,12 @@ class ShippingAddressSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+
 class OrderSerializer(serializers.ModelSerializer):
     orderItems = serializers.SerializerMethodField(read_only=True)
     shippingAddress = serializers.SerializerMethodField(read_only=True)
@@ -26,7 +32,7 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = "__all__"
 
-    def get_orders(self, obj):
+    def get_orderItems(self, obj):
         items = obj.orderitem_set.all()
         serializer = OrderItemSerializer(items, many=True)
         return serializer.data
@@ -44,12 +50,6 @@ class OrderSerializer(serializers.ModelSerializer):
         return address
 
 
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = "__all__"
-
-
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
     _id = serializers.SerializerMethodField(read_only=True)
@@ -57,7 +57,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "_id", "username", "email", "is_admin"]
+        fields = ["id", "_id", "username", "email", "is_admin", "name"]
 
     def get_name(self, obj):
         name = obj.first_name
